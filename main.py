1f5aff0c48491f2955e33bcba92c92d46e5b6e61
SnowyMinion <rm.cassilam@gmail.com>
2017-07-24 16:30:46 -0700

    Added logo

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class Question(ndb.Model):
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template("templates/homepage.html")
-
+        
         # Creates the user login.
         current_user = users.get_current_user()
         logout_url = users.create_logout_url('/')
