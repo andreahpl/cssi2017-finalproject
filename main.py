843a43a8de0e39963db799133d8f7470d8f9cbef
Chris Avalos <ca44872@gmail.com>
2017-07-24 12:13:44 -0700

    Added the models and edited the main handler.

--- main.py
+++ main.py
@@ -15,10 +15,48 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
+
+# Import the database API.
+from google.appengine.ext import ndb
+
+# Import the user built-in API.
+from google.appengine.api import users
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+# This is our model for our user.
+class User(ndb.Model):
+    username = ndb.StringProperty()
+    email = ndb.StringProperty()
+    age = ndb.IntegerProperty()
+
+# This is our model for our questions.
+class Question(ndb.Model):
+    questionText = ndb.StringProperty()
+    answer = ndb.StringProperty()
+    category = ndb.ReferenceProperty()
+    difficulty = ndb.ReferenceProperty()
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+        template = jinja_environment.get_template(#THEHTMLFILE)
+
+        # Creates the user login.
+        current_user = users.get_current_user()
+        logout_url = users.create_logout_url('/')
+        login_url = users.create_login_url('/')
+
+        # This dictionary stores the variables.
+        template_vars = {
+        'current_user': current_user,
+        'logout_url': logout_url,
+        'login_url': login_url
+        }
+    self.response.write(template.render(template_vars))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
