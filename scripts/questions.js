f149ed1c9068deea5d9a406f38948600c084fc00
Chris Avalos <ca44872@gmail.com>
2017-07-26 16:33:08 -0700

    Added a score function

--- scripts/questions.js
+++ scripts/questions.js
@@ -6,3 +6,28 @@ function advanceQuestion() {
     current_question += 1;
     questions[current_question].style.display = 'block';
 }
+
+function changeScore() {
+  // Here, "this" is the button that the user clicked.
+  var button = $(this);
+
+  // Move through the DOM tree to find the "likes"
+  // element that corresponds to the clicked button.
+
+  // Look through parents of this to find .photo.
+  var user = $(this).parents('.user');
+
+  // Look inside photo to find .likes.
+  var scores = $(user).find('.scores');
+
+  // Get the URLsafe key from the button value.
+  var urlsafeKey = $(button).val();
+
+  // Send a POST request and handle the response.
+    $.post('/likes', {'photo_key': urlsafeKey}, function(response) {
+      // Update the number in the "like" element.
+      $(likes).text(response);
+    });
+  }
+
+  $('.photo button').click(clickLike);
